da134b77765ad82ccf5480d744c38d4da9339ba5
HYJJJJJJJJ <hyj1998082724@gmail.com>
2020-06-29 00:45:41 +0800

    6.29 删除自动生成的多余注释
    Signed-off-by: HYJJJJJJJJ <hyj1998082724@gmail.com>

--- Example/LED_Flash_Example/Core/Inc/gpio.h
+++ Example/LED_Flash_Example/Core/Inc/gpio.h
@@ -1,43 +1,17 @@
-/**
-  ******************************************************************************
-  * File Name          : gpio.h
-  * Description        : This file contains all the functions prototypes for 
-  *                      the gpio  
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+/*
+ * gpio.h
+ *
+ *  Created on: Jun 29, 2020
+ *      Author: HYJ
+ */
 
-/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __gpio_H
 #define __gpio_H
 #ifdef __cplusplus
  extern "C" {
 #endif
 
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* USER CODE BEGIN Private defines */
-
-/* USER CODE END Private defines */
-
-void MX_GPIO_Init(void);
-
-/* USER CODE BEGIN Prototypes */
 
 typedef struct GPIO_COMMON{
 	GPIO_InitTypeDef GPIO_InitStruct;
@@ -48,19 +22,9 @@ typedef struct GPIO_COMMON{
 }GPIO_COMMON;
 //GPIO类初始化
 GPIO_COMMON *new_Gpio(struct GPIO_COMMON *this,GPIO_TypeDef  *port,uint32_t pin,uint32_t mode,uint32_t pull,uint32_t speed);
-/* USER CODE END Prototypes */
 
 #ifdef __cplusplus
 }
 #endif
-#endif /*__ pinoutConfig_H */
+#endif
 
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
